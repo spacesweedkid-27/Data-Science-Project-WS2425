1dfc2114b0fbfa3c492856295f49177331f7b3fc
Henri Heyden <henri@heyden5.de>
2025-03-11 12:00:10 +0100

    Modified comments and doc for PEP 8 and 257 + new method that checks if a chord is a simple valid chord.

--- data collection/scripts/numerize chords.py
+++ data collection/scripts/numerize chords.py
@@ -1,47 +1,65 @@
 #!/usr/bin/env python
 
+from re import search as research
+
+
+# This may be useful when pre-processing the chords of the songs
+# to check if some chords are simple chords.
+def is_valid_chord(chord: str) -> bool:
+    """Returns True if the chord provided is a real chord in short or long chord notation.
+       It should be advised that some complex chords in the long chord notation like "Cmajm7",
+       - the dominant seventh accord for C, would be flagged as wrong."""
+    # The regular expression searches for chords that start with a note,
+    # then specify if they are flat or sharp, if their mode is correct,
+    # then if they add any more notes like the seventh, and finally,
+    # if the chord is a slash-chord, which means that an additional bass is played.
+    return research("^[A-G][b#]?(m|maj|min|dim|aug)?[0-9]*(/[A-G][#b]?)?$", chord) is not None
+
+
 def convert_to_position_at_keyboard(chord: str) -> int:
-    """converts the note of a chord to where it is on the keyboard
+    """Converts the note of a chord to where it is on the keyboard.
     """
-    # first convert the note to the corresponding position on the scale
+    # First convert the note to the corresponding position on the scale.
     temp = {'C':0, 'D':2, 'E':4, 'F':5, 'G':7, 'A':9, 'B':11}[chord[0]]
     
-    # if the string ends there, terminate
+    # If the string ends there, terminate.
     if len(chord) <= 1:
         return temp % 12
 
-    # if the first note is sharp or flat, adjust the result accordingly
+    # If the first note is sharp or flat, adjust the result accordingly.
     sharp = chord[1] == '#'
     flat = chord[1] == 'b'
-    # since a note can't be flat and sharp at the same time, this is fine
+    # Since a note can't be flat and sharp at the same time, this is fine.
     temp += sharp - flat
 
     return temp % 12
 
 
 def is_minor(chord: str) -> bool:
-    """returns if a chord is a minor chord"""
-    # since in the notation the chords is exactly in minor
+    """Returns if a chord is a minor chord."""
+    # Since in the notation the chords is exactly in minor,
     # if the char after the key position is 'm', we can do this,
-    # since 'm' is not in a Chord notation at any other position
-    # for example 'C/Gm' is not a valid chord,
+    # since 'm' is not in a Chord notation at any other position.
+    # For example 'C/Gm' is not a valid chord,
     # since 'C/G' is C-mayor with G bass. Alternatively 'Gm/C'
     # would be correct, being G-minor with C bass.
     return  'min' in chord \
             or 'm' in chord \
             and 'dim' not in chord \
+            and 'aug' not in chord \
             and 'maj' not in chord
 
 
+# Currently unused, but could be used later for data visualisation.
 def main_chord_to_transpose(key: str) -> int:
     """Calculates the amount of half-steps that should be added
        to transpose the key signature to C-mayor or A-minor, 
-       if the signature is in minor mode or not
+       if the signature is in major or minor mode.
     """
-    # get the postion on the scale
+    # Get the postion on the scale.
     temp = convert_to_position_at_keyboard(key)
 
-    # check if the chord is in major or minor
+    # Check if the chord is in major or minor
     # if it is, we want to transpose to A-minor not C-minor
     temp += 3 * is_minor(key)
     
@@ -50,44 +68,43 @@ def main_chord_to_transpose(key: str) -> int:
 
 
 def convert_letter_to_harmonic_position(key: str, chord: str) -> int:
-    """assuming a chord is played in the context of some key,
+    """Assuming a chord is played in the context of some key,
        returns the position on the corresponding harmonic scale,
        where the chord is placed.
        E.g. for A-minor and Bdim it should return 2,
-       since the diminished B-chord is the second chord in the A-minor scale"""
+       since the diminished B-chord is the second chord in the A-minor scale."""
     index_origin = convert_to_position_at_keyboard(key)
     index_chord = convert_to_position_at_keyboard(chord)
 
     diff = index_chord - index_origin
     
     if not is_minor(key):
-        # "0" means that the chord is not in the major harmony
+        # "0" means that the chord is not in the major harmony.
         return [1,0,2,0,3,4,0,5,0,6,0,7][diff]
     else:
-        # "0" means that the chord is not in the minor harmony
+        # "0" means that the chord is not in the minor harmony.
         return [1,0,2,3,0,4,0,5,6,0,7,0][diff]
 
 
 def shrink_chord(chord: str) -> str:
-    """shrinks the chord so, that its main mode is only passed
-       diminished chords count as their major chord in this implementation
-       examples: "Gmaj" -> "G", "Bb" -> "Bb", "C#min" -> "C#m"
-
+    """Shrinks the chord so, that its main mode is only passed
+       diminished and augmented chords count as major chord in this implementation.
+       Examples: "Gmaj" -> "G", "Bb" -> "Bb", "C#min" -> "C#m".
     """
     if len(chord) <= 2:
         return chord
     else:
-        # check if there is another important symbol to include
-        is_fos = 'b' in chord or '#' in chord
-        # if it is minor, we want to include the m, if not then not.
+        # Check if there is another important symbol to include.
+        has_flatsharp = chord[1] in ['b', '#']
+        # If it is minor, we want to include the m, if not then not.
         if is_minor(chord):
-            return chord[:2 + is_fos]
+            return chord[:2 + has_flatsharp]
         else:
-            return chord[:1 + is_fos]
+            return chord[:1 + has_flatsharp]
 
 
 def identify_key(chords: list[str], method: int = 0) -> str:
-    """according to what chords are in a song, returns the key according to some method
+    """According to what chords are in a song, returns the key according to some method.
        Both methods may be wrong. For example: "Creep" by Radiohead is in G major,
        but the most frequent accord contains Cs. The first method counts C and Cm as different chords,
        so the algorithm still determines G is the right key, since it is the first chord,
@@ -95,26 +112,27 @@ def identify_key(chords: list[str], method: int = 0) -> str:
        Additionally there are many songs that start with another chord and not the tonic chord.
     """
     if method == 0:
-        # first method sort by occurrences of individual chords
-        # and select most frequent one
+        # First method: Sort by occurrences of individual chords,
+        # and select the most frequent one.
         nums = {}
-        # calculates frequencies of all shrinked chords
+        # Calculates frequencies of all shrinked chords.
         for chord in [shrink_chord(c) for c in chords]:
             if chord not in nums.keys():
                 nums[chord] = 1
             else:
                 nums[chord] += 1
         
+        # Search for chord with maximal occurrences.
         return max(chords, key=lambda c: nums[c])
     elif method == 1:
-        # select first chord as tonic chord
+        # Second method: Select first chord as tonic chord.
         return shrink_chord(chords[0])
     else:
-        raise Exception("Wrong argument for method parameter")
+        raise ValueError("Wrong argument for method parameter")
 
 
 def convert_song_to_harmony(chords: list[str], method: int = 0) -> list[int]:
-    """identifies the key and converts all chords to their harmonic according to that key"""
+    """Identifies the key and converts all chords to their harmonic according to that key."""
     key = identify_key(chords, method)
     return [convert_letter_to_harmonic_position(key, chord) for chord in chords]
 
