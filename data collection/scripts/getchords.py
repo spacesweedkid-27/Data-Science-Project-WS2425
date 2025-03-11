433d8f1d441102f1c506c88e3e6a5f1ebff547f3
Amaroq <nike-marie@hotmail.de>
2025-03-11 08:27:44 +0100

    getchords.py now works, accessing ultimate-guitar directly. Contains some assumptions about result structure which could lead to inaccurate results, we'll have to look into that. I also haven't run this script on large amounts of data so I can't be sure whether I can be locked out of UG as well, if requests happen to frequently. There's some measures to prevent that already integrated. #7

--- data collection/scripts/getchords.py
+++ data collection/scripts/getchords.py
@@ -1,13 +1,12 @@
 import csv
 import random
 import requests
-from bs4 import BeautifulSoup
 import re
 
 # not optimal, google quickly blocks
-def google_search(song, artist):
-    query = f'site:ultimate-guitar.com "{song}" "{artist} chords'
-    url = f'https://google.com/search?q={query}'
+def search(song, artist):
+    query = f'"{song}" "{artist}'
+    url = f'https://ultimate-guitar.com/search.php?search_type=title&value={query} chords'
 
     # maybe this helps with getting ip-banned?
     headers = {
@@ -23,18 +22,21 @@ def google_search(song, artist):
         print(f'failed to fetch search results for {song} by {artist}')
         return None
     
-    soup = BeautifulSoup(response.text, "html.parser")
+    '''Ultimate Guitar hides the links within JSON structures on load, so instead of
+    accessing them through their a href tag we'll need to look for the appropriate
+    link structure in the returned JSON. Currently this works under the assumption
+    that the first link found is always a "chords"-tab, as this is the most common
+    search-result, but especially for older songs this might vary. A solution might
+    be to also check whether "chords" is somewhere within the link, as it usually is,
+    but I can't be 100% sure about this right now. The next steps in the processing-workflow
+    will reveal if I need to refine a couple of things.'''
 
-    # get first valid ultimate-guitar link (publicly accessible, must be chords)
-    for link in soup.find_all("a", href=True):
-        href = link["href"]
+    match = re.search(r'https://tabs\.ultimate-guitar\.com/tab/[^&]*', response.text)
 
-        match = re.search(r"/url\?q=(https://tabs.ultimate-guitar.com/[\S]+)&", href)
-        if match:
-            tab_url = match.group(1)
-            # make sure it's not a pro tab (pro tabs usually have /pro/ in their url)
-            if "/pro/" not in tab_url:
-                return tab_url
+    if match:
+        return match.group(0)
+        
+    return None
 
 def getUGlinks(input, output):
     # read chart csv, copy info to new csv and add link
@@ -42,13 +44,20 @@ def getUGlinks(input, output):
         reader = csv.DictReader(csvfile)
         songs = list(reader)
 
+        # results list to buffer title, artist and links
         results = []
         for song in songs:
             song_name = song["Title"]
             artist_name = song["Artist(s)"]
 
             print(f'Searching for {song_name} by {artist_name}')
-            tab_url = google_search(song_name, artist_name)
+            '''currently it looks like UG doesn't ban automatic queries as quickly,
+            but I haven't found reliable information about that yet and have only tested
+            with a small amount of queries (5-7 per test run), which might also be
+            the reason I haven't been banned yet. A sleep timer might be a solution
+            but for testing it just took too long.'''
+            #time.sleep(random.randint(10,100)) # randomly sleep between queries to not get locked out
+            tab_url = search(song_name, artist_name)
 
             results.append({
                 "Title": song_name,
@@ -56,6 +65,7 @@ def getUGlinks(input, output):
                 "UG_link": tab_url if tab_url else "not found"
             })
 
+        # write result into new csv
         with open(output, "w", newline="", encoding="utf-8") as csvfile:
             fieldnames = ["Title", "Artist", "UG_link"]
             writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
@@ -63,7 +73,6 @@ def getUGlinks(input, output):
             writer.writerows(results)
         print(f'results saved for {output}')
 
-print(google_search("Lose Control", "Teddy Swims"))
-print(google_search("Someone Like You", "Adele"))
 
+# it might be useful to add automation to this process as well
 getUGlinks("billboard_2024.csv", "billboard_2024_chords.csv")
