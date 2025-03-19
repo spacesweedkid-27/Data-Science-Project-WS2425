f6231711061e903295e0ea8ec47ebc085406aec4
Nike <nikepulow@outlook.de>
2025-03-19 10:05:22 +0100

    about content wip #17

--- pages/about.py
+++ pages/about.py
@@ -6,11 +6,48 @@ import dash_bootstrap_components as dbc
 
 dash.register_page(__name__)
 
-heading = dbc.Container('About this project')
-main_content = dbc.Container('Some information about this project goes here. '
-'This content answers questions about what data sources we used, how the data '
-'was processed and what assumptions were made. Whilest this information not '
-'being listed in the official requirements provided by CAU, it still holds a lot'
-'of value from a scientific standpoint.')
+about_heading = dbc.Container('About this project', class_name='h2 mt-5')
+introduction = dbc.Container('This website is part of a university research '
+'project analyzing trends in popular music over the past two decades. It was '
+'conducted by a group of computer science students. Our research questions focus '
+'on identifying patterns in musical composition, lyrical themes, and genre '
+'evolution from 2005 to 2024.', class_name='mb-2')
+questions_heading = dbc.Container('the questions', class_name='h3 mt-4')
+questions = dbc.Container('The questions go here.', class_name='mb-2')
+datasources_heading = dbc.Container('datasources', class_name='h3 mt-4')
+datasources = dbc.Container(
+    ['To explore these questions, we gathered data from multiple sources: ',
+        html.Ul([
+            html.Li([
+                html.Strong('Billboard Year-End Hot 100 (2005-2024): '),
+                'provided the top 100 most commercially successful songs each year.'
+            ], className='mb-2'),
+            html.Li([
+                html.Strong('Ultimate Guitar: '),
+                'used to retrieve chords progressions. Some songs were unavailable '
+                'due to licensing issues.'
+            ], className='mb-2'),
+            html.Li([
+                html.Strong('songbpm.com: '),
+                'provided tempo (beats per minute) data.'
+            ], className='mb-2'),
+            html.Li([
+                html.Strong('Genius API: '),
+                'used to collect lyrics for textual and thematic analysis.'
+            ], className='mb-2'),
+            html.Li([
+                html.Strong('last.fm API: '),
+                'extracted genre classifications using top user-generated tags.'
+            ], className='mb-2')
+        ], className='mt-3')
+    ]
+)
 
-layout = html.Div([heading, main_content])
+layout = html.Div([
+    about_heading,
+    introduction,
+    questions_heading,
+    questions,
+    datasources_heading,
+    datasources
+    ])
